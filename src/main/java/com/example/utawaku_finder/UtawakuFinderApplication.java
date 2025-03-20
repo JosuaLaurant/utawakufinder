f9b5135db21621765925595e78e20f91a5278606
jiho <jiho@DESKTOP-LAHR80K>
2025-03-20 12:44:11 +0900

    spring initial setting

---
+++ src/main/java/com/example/utawaku_finder/UtawakuFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.utawaku_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtawakuFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtawakuFinderApplication.class, args);
+	}
+
+}
